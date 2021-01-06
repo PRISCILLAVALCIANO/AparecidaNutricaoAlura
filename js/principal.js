313b877d2dca2e11797cd58bb366d0b7ad61b536
Priscilla Valciano <priscilla.valciano@hotmail.com>
2021-01-06 17:01:56 -0300

    Recplicando a validacao e calculo do IMC e limitando as casas decimais
    Recplicando a validacao e calculo do IMC para todos os pacientes da tabela e limitando as casas decimais do resultado do IMC

--- js/principal.js
+++ js/principal.js
@@ -2,36 +2,43 @@ var titulo = document.querySelector(".titulo");
     titulo.textContent = "Aparecida Nutricionista";
 
 // Obtentando os dados do peso e altura e calculando o IMC do primeiro paciente
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var pesoValido = true;
-var alturaValida = true;
-    
+    var pesoValido = true;
+    var alturaValida = true;
+        
 
-//Validando campos de peso e altura de
+    //Validando campos de peso e altura de
 
-if(peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido, revise o cadastro!");
-    pesoValido = false;
-    tdImc.textContent = "Peso inválido!"
+    if(peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido, revise o cadastro!");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!"
+    }
+
+    if(altura <= 0 || altura >= 2.50) {
+        console.log("Altura inválida, revise o cadastro!");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!"
+    }
+
+    if(pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
 }
 
-if(altura <= 0 || altura >= 2.50) {
-    console.log("Altura inválida, revise o cadastro!");
-    alturaValida = false;
-    tdImc.textContent = "Altura inválida!"
-}
-
-if(pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
