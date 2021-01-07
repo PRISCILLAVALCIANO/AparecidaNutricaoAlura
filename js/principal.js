c4543a16cbf56b531b539ced9d7cb0a8fa5b45dd
Priscilla Valciano <priscilla.valciano@hotmail.com>
2021-01-06 22:34:41 -0300

    Criação do formulario e adicionando pacientes na tabela
    Criação do formulario e adicionando pacientes na tabela

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,7 @@
 var titulo = document.querySelector(".titulo");
     titulo.textContent = "Aparecida Nutricionista";
 
-// Obtentando os dados do peso e altura e calculando o IMC do primeiro paciente
+// Obtentando os dados do peso e altura e calculando o IMC dos pacientes
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var i = 0; i < pacientes.length; i++) {
@@ -42,4 +42,40 @@ for(var i = 0; i < pacientes.length; i++) {
     }
 
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
+
 
